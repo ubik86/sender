c8569f5257370fe61790281e81e152a6d2efd746
Boguslaw Tarnowski <boguslaw.tarnowski@intive.com>
2018-01-19 14:17:59 +0100

    mail form, subpage send with axios

--- routes/index.js
+++ routes/index.js
@@ -4,18 +4,25 @@ var axios = require('axios');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  axios.post('http://192.168.1.252:8080/api/mail', {
-    email: 'boguslaw.tarnowski@intive.com',
-    topic: 'test',
-    message: 'its working'
-  })
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/send', function(req, res, next) {
+  axios.post('http://192.168.1.252:8080/api/mail', 
+    req.query
+  // {
+  //   email: 'boguslaw.tarnowski@intive.com',
+  //   topic: 'test',
+  //   message: 'its working'
+  // }
+  )
   .then(function (response) {
     console.log('sended');
+    res.render('result', { title: 'Express' });
   })
   .catch(function (error) {
     console.log(error);
   });
-  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
